5eab4090a31f68f762088a6be7af6fe857e39db9
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-03-26 23:30:10 +0900

    updated user validation, user parameter, and login/register templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,   presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "お名前は全角（漢字・ひらがな・カタカナ）での入力が必須。" }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "苗字は全角（漢字・ひらがな・カタカナ）での入力が必須。" }
+  validates :phonetic_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "お名前のフリガナはカタカナでの入力が必須。" }
+  validates :phonetic_last_name,  format: { with: /\A[ァ-ヶー－]+\z/, message: "苗字のフリガナはカタカナでの入力が必須。" }
+  validates :birthday, presence: true
 end
