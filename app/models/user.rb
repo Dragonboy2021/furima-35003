d0a968bf280e5adf2973c5c6f937401b72b394f2
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-03-28 21:36:24 +0900

    fixed model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  name_reg = /\A[ぁ-んァ-ン一-龥々ー]+\z/
+  phonetic_reg = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合でなければ登録できない' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）での入力が必須。' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）での入力が必須。' }
-    validates :phonetic_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナでの入力が必須。' }
-    validates :phonetic_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナでの入力が必須。' }
+    validates :first_name, format: { with: name_reg, message: 'は全角（漢字・ひらがな・カタカナ）での入力が必須。' }
+    validates :last_name, format: { with: name_reg, message: 'は全角（漢字・ひらがな・カタカナ）での入力が必須。' }
+    validates :phonetic_first_name, format: { with: phonetic_reg, message: 'はカタカナでの入力が必須。' }
+    validates :phonetic_last_name, format: { with: phonetic_reg, message: 'はカタカナでの入力が必須。' }
     validates :birthday
   end
 end
