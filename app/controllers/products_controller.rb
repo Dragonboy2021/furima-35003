07d5f5aa7ea5d9625fbfc1432529a157b322f0c6
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-03-30 21:24:57 +0900

    added product edit, update page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_product, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @products = Product.order('created_at DESC')
@@ -19,7 +21,19 @@ class ProductsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
     @product = Product.find(params[:id])
+
+    if @product.update(product_param)
+      redirect_to product_path(product_param)
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,5 +41,16 @@ class ProductsController < ApplicationController
   def product_param
     params.require(:product).permit(:image, :title, :info, :category_id, :quality_id, :delivery_fee_id, :location_id,
                                     :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def move_to_index
+    product = Product.find(params[:id])
+    unless current_user && product.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
