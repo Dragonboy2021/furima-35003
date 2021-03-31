e6ca0cfc2e63832447ed6c848679846fb00416e0
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-03-31 11:04:37 +0900

    added product destroy method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.order('created_at DESC')
@@ -34,6 +34,11 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
   def product_param
@@ -46,8 +51,6 @@ class ProductsController < ApplicationController
   end
 
   def move_to_index
-    unless @product.user_id == current_user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless @product.user_id == current_user.id
   end
 end
