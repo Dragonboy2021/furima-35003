729a83106095566b6c895e46fa57a96bb0f55066
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-04-02 12:29:57 +0900

    added models and 単体テスト

---
+++ app/models/order_shipping_address.rb
@@ -0,0 +1,22 @@
+class OrderShippingAddress
+  include ActiveModel::Model
+  attr_accessor :postal_address, :state_id, :city, :street, :building, :phone, :user_id, :product_id, :order_id, :token
+
+  with_options presence: true do
+    validates :postal_address, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :state_id, numericality: { other_than: 0, message: 'must be selected' }
+    validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/, message: "is invalid"}
+    validates :city, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "is invalid"}
+    validates :street
+    validates :token
+    validates :user_id
+    validates :product_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, product_id: product_id, token: token)
+    ShippingAddress.create(postal_address: postal_address, state_id: state_id, city: city, street: street, building: building, order_id: order.id)
+  end
+
+
+end
